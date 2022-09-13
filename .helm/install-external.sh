4008a6d5b54c3be33f66060726c97f7a168626d0
skwinnik <screen0994@gmail.com>
2022-09-13 17:32:38 +0000

    persistence settings

--- .helm/install-external.sh
+++ .helm/install-external.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 helm upgrade -i rabbitmq bitnami/rabbitmq -f rabbitmq/values.yaml --namespace async-course
-helm upgrade -i postgresql bitnami/postgresql --namespace async-course
-helm upgrade -i kafka bitnami/kafka --namespace async-course
+helm upgrade -i postgresql bitnami/postgresql -f postgre/values.yaml --namespace async-course
+helm upgrade -i kafka bitnami/kafka -f kafka/values.yaml --namespace async-course
