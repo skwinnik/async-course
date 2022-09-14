a96671db470cae0fc5f97d2e52f16e7766ccd7ec
skwinnik <screen0994@gmail.com>
2022-09-14 12:22:28 +0000

    add kafdrop

--- .helm/install-external.sh
+++ .helm/install-external.sh
@@ -3,3 +3,4 @@
 helm upgrade -i rabbitmq bitnami/rabbitmq -f rabbitmq/values.yaml --namespace async-course
 helm upgrade -i postgresql bitnami/postgresql -f postgre/values.yaml --namespace async-course
 helm upgrade -i kafka bitnami/kafka -f kafka/values.yaml --namespace async-course
+helm upgrade -i kafdrop rhcharts/kafdrop -f kafdrop/values.yaml --namespace async-course
