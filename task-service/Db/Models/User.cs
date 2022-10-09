279e17a64225430aac1ac85ea27bdc6ff0ec5128
skwinnik <screen0994@gmail.com>
2022-10-09 15:47:28 +0000

    fix userId autogen

--- task-service/Db/Models/User.cs
+++ task-service/Db/Models/User.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace TaskService.Db.Models {
   public class User {
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
     public Guid UserId { get; set; } = Guid.Empty;
     public string UserName { get; set; } = "";
     public string RoleName { get; set; } = "";
